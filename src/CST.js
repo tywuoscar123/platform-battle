ab602ab986459c92cc2bcc89135487727926e889
TAMTszChung <tctam@connect.ust.hk>
2021-03-30 14:33:55 +0800

    add devil goal and add spawn point

--- src/CST.js
+++ src/CST.js
@@ -3,5 +3,8 @@ export const CST = {
         MENU: "MENU",
         GAME: "GAME",
         END: "END"
+    },
+    CONFIG: {
+        TIMER: 60*1000
     }
 }
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -45,8 +45,10 @@ export default class GameScene extends Phaser.Scene {
         this.platformLayer = this.castleMap.createLayer('Platform', 'Castletiles');
         this.platformLayer.setCollisionByProperty({collides:  true});
 
+        this.spawnPt = this.castleMap.findObject('Objects', obj => obj.name === 'Spawn');
+        this.goal = this.castleMap.findObject('Objects', obj => obj.name === 'Goal');
         //create the player character and add physics to player
-        this.wizard = new Player(this, 25, 580);
+        this.wizard = new Player(this, this.spawnPt.x, this.spawnPt.y);
         this.physics.world.addCollider(this.wizard.sprite, this.platformLayer);
 
         //add in obstacles spikes
@@ -59,11 +61,11 @@ export default class GameScene extends Phaser.Scene {
         //add mouse click tracer for hero player
         this.tracer = new MouseTracer(this, this.castleMap);
 
-        //add Add state for game over checking
+        //add state for game over checking
         this.gameover = false;
 
         //add timer for stage
-        this.timer = this.time.delayedCall( 5*1000, this.HeroWin, null, this);
+        this.timer = this.time.delayedCall( CST.CONFIG.TIMER, this.HeroWin, null, this);
         this.text = this.add.text(5, 20, 'Remaining Time: ', { font: 'bold 12px system-ui' });
     }
 
@@ -80,14 +82,16 @@ export default class GameScene extends Phaser.Scene {
 
         //update remaining time
         this.text.setText('Remaining Time: ' + this.timer.getRemainingSeconds().toFixed(1));
+        if (this.wizard.sprite.x > this.goal.x && this.wizard.sprite.y > this.goal.y){
+            this.DevilWin();
+        }
     }
 
     restart() {
         //reset the devil player position
-        console.log("restart");
         this.wizard.sprite.setVelocity(0, 0);
-        this.wizard.sprite.setX(25);
-        this.wizard.sprite.setY(590);
+        this.wizard.sprite.setX(this.spawnPt.x);
+        this.wizard.sprite.setY(this.spawnPt.y);
         this.wizard.sprite.play('Idle', true);
         this.wizard.sprite.setAlpha(0);
         this.tweens.add({
