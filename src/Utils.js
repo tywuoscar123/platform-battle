1e36096ea811cdcfae30d0fe11aa798587f5ab4b
TAMTszChung <tctam@connect.ust.hk>
2021-04-01 15:40:02 +0800

    add createTextButton function

--- src/Utils.js
+++ src/Utils.js
@@ -4,5 +4,15 @@ export default class Utils{
     }
 
     createTextButton(x, y, text, style, originalColor, overColor, callback){
+        let textButton = this.GameScene.add.text(x, y, text, style).setOrigin(0.5);
+        textButton.setInteractive(new Phaser.Geom.Rectangle(0, 0, textButton.width, textButton.height), Phaser.Geom.Rectangle.Contains);
+        textButton.on('pointerover', function() {
+            this.setFill(overColor);
+        });
+        textButton.on('pointerout', function() {
+            this.setFill(originalColor);
+        });
+        textButton.on('pointerdown', callback, this.GameScene);
+        return textButton;
     }
 }
--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -1,12 +1,12 @@
 import { CST } from "../CST";
-import {game} from "../index";
-import {saves} from "../saves";
+import Utils from "../Utils";
 
 export default class MenuScene extends Phaser.Scene {
     constructor() {
         super({
             key: CST.SCENES.MENU
         })
+        this.utilfunctions = new Utils(this);
     }
     init(){
     }
@@ -24,16 +24,14 @@ export default class MenuScene extends Phaser.Scene {
         this.add.text(screenCenterX, screenCenterY - 100, "Platform Battle: Devil vs Hero", { font: "65px Arial", fill: "#ffffff" }).setOrigin(0.5);
 
         //add play button
-        let text = this.add.text(screenCenterX, screenCenterY+100, "Play", { font: "65px Arial", fill: "#ff0044"}).setOrigin(0.5);
-        text.setInteractive(new Phaser.Geom.Rectangle(0, 0, text.width, text.height), Phaser.Geom.Rectangle.Contains);
-        text.on('pointerover', function() {
-            this.setFill("#ffffff");
-        });
-        text.on('pointerout', function() {
-            this.setFill("#ff0044");
-        });
-        text.on('pointerdown', function() {
-            this.scene.start(CST.SCENES.GAME);
-        }, this);
+        this.utilfunctions.createTextButton(screenCenterX,
+            screenCenterY+100,
+            "Play",
+            { font: "65px Arial", fill: "#ff0044"},
+            "#ff0044",
+            "#ffffff",
+            function() {
+                this.scene.start(CST.SCENES.GAME);
+            });
     }
 }
