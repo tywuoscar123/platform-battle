c64351c486dc4d2e0aaa0ea1fe63bcc804205eb5
TAMTszChung <tctam@connect.ust.hk>
2021-04-01 15:01:45 +0800

    add Utils class

---
+++ src/Utils.js
@@ -0,0 +1,8 @@
+export default class Utils{
+    constructor(GameScene) {
+        this.GameScene = GameScene;
+    }
+
+    createTextButton(x, y, text, style, originalColor, overColor, callback){
+    }
+}
