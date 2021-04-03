c3295282575b1edd20d2336de77494c03dbff710
Oscar <tywu.oscar@gmail.com>
2021-04-03 14:29:27 +0800

    added cannon, magicorb, cannonball, and beartrap sprites and animation

---
+++ src/objects/Beartrap.js
@@ -0,0 +1,31 @@
+import Phaser from "phaser";
+import { CST } from "../CST";
+
+export default class Beartrap extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y, texture = 'beartrap', frame = 0) {
+        super(scene, x, y, texture, frame);
+
+        const anims = this.scene.anims;
+        anims.create({
+            key: 'activate',
+            frames: 'beartrap',
+            frameRate: 5,
+            repeat: -1
+        });
+
+ 
+
+        //set player properties
+        this.scene.add.existing(this);
+        this.scene.physics.add.existing(this);
+
+        this.setOrigin(0.5, 0.5);
+
+        this.setScale(1.5, 1.5);
+        this.play("activate", true);
+    }
+
+    update(args) {
+    }
+
+}
---
+++ src/objects/Cannonball.js
@@ -0,0 +1,24 @@
+import Phaser from "phaser";
+import { CST } from "../CST";
+
+export default class Cannonball extends Phaser.GameObjects.Sprite{
+    constructor(scene, x, y, texture = 'cannonball', frame = 0) {
+        super(scene, x, y, texture, frame);
+
+        //set sprite properties
+        this.scene.add.existing(this);
+        this.scene.physics.add.existing(this);
+
+        this.setOrigin(0.5, 0.5);
+        this.setScale(0.5, 0.5);
+        this.body.setCollideWorldBounds(true);
+
+
+    }
+
+    update(args) {
+        let Fx = 0;
+        let Fy = 0;
+    }
+
+}
