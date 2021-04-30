541ab646c8758590e724f0d5e5f4d78064fdd006
TAMTszChung <tctam@connect.ust.hk>
2021-04-30 22:04:38 +0800

    update tutorial scene

--- src/scenes/TutorialScene.js
+++ src/scenes/TutorialScene.js
@@ -19,6 +19,7 @@ export default class TutorialScene extends Phaser.Scene {
     }
 
     preload(){
+        this.load.image('ruin', 'assets/menuBG/ruin.png');
     }
 
     /**
@@ -29,30 +30,46 @@ export default class TutorialScene extends Phaser.Scene {
         const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
         const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
 
+        //load background image
+        let BG = this.add.image(screenCenterX,screenCenterY,'ruin');
+        BG.displayWidth = this.sys.canvas.width;
+        BG.displayHeight = this.sys.canvas.height;
+
         let originalColor = "#00ff00";
         let overColor = "#ffffff";
 
         let buttonOffsetY = 130;
 
+        let devilColor = "#ff0000"
+        let heroColor = "#00ff00"
+
         //add title
         let title = this.add.text(screenCenterX, 80, "How to Play", { font: "65px Arial", fill: "#ffffff" }).setOrigin(0.5);
+
         //text for devil tutorial
-        this.add.text(220, 150, " Devil Controls", { font: "40px Arial", fill: "#ffffff" });
-        this.add.text(120, 220, "A - Go forward", { font: "25px Arial", fill: "#ffffff" });
-        this.add.text(120, 280, "D - Go backwards", { font: "25px Arial", fill: "#ffffff" });
-        this.add.text(120, 340, "W - Jump", { font: "25px Arial", fill: "#ffffff" });
-        this.add.text(400, 220, "1 - Activate Super Jump", { font: "25px Arial", fill: "#ffffff" });
-        this.add.text(400, 280, "2 - Activate Super Speed", { font: "25px Arial", fill: "#ffffff" });
-        this.add.text(400, 340, "3 - Heal", { font: "25px Arial", fill: "#ffffff" });
-        this.add.text(400, 400, "4 - Reload Magic Orb", { font: "25px Arial", fill: "#ffffff" });
-        this.add.text(80, 460, "Goal: Reach the end of the map to defeat the Hero", { font: "25px Arial", fill: "#ffffff" });
+        this.add.text(200, 120, "Devil Controls", { font: "40px Arial", fill: devilColor});
+        this.add.text(80, 180, "Controls (Keyboard): ", { font: "25px Arial", fill: devilColor});
+
+        this.add.text(80, 220, "A - Go forward", { font: "25px Arial", fill: devilColor });
+        this.add.text(80, 280, "D - Go backwards", { font: "25px Arial", fill: devilColor });
+        this.add.text(80, 340, "W - Jump", { font: "25px Arial", fill: devilColor });
+
+        this.add.text(360, 220, "1 - Activate Super Jump", { font: "25px Arial", fill: devilColor });
+        this.add.text(360, 280, "2 - Activate Super Speed", { font: "25px Arial", fill: devilColor });
+        this.add.text(360, 340, "3 - Heal", { font: "25px Arial", fill: devilColor });
+        this.add.text(360, 400, "4 - Reload Magic Orb", { font: "25px Arial", fill: devilColor });
+
+        this.add.text(80, 460, "Goal: Control the devil avatar and reach the bottom right goal of each level within the time limit to defeat the Hero", { font: "25px Arial", fill: devilColor, wordWrap: { width: 600 } });
 
         //text for hero tutorial 
-        this.add.text(1000, 150, " Hero Controls", { font: "40px Arial", fill: "#ffffff" });
-        this.add.text(900, 220, "Left click on map to select trap location", { font: "25px Arial", fill: "#ffffff" });
-        this.add.text(900, 280, "Select traps on the right of the screen", { font: "25px Arial", fill: "#ffffff" });
-        this.add.text(900, 340, "Click on the traps to drop them", { font: "25px Arial", fill: "#ffffff" });
-        this.add.text(780, 460, "Goal: Stop the Devil from reaching the end of the map", { font: "25px Arial", fill: "#ffffff" });
+        this.add.text(900, 120, " Hero Controls", { font: "40px Arial", fill: heroColor });
+        this.add.text(800, 180, "Controls (Mouse): ", { font: "25px Arial", fill: heroColor});
+
+        this.add.text(800, 220, "In each level, first, Left click on map to select a location (indicated by white square)", { font: "25px Arial", fill: heroColor, wordWrap: { width: 500 } });
+        this.add.text(800, 310, "Then, select a trap on the right of the screen", { font: "25px Arial", fill: heroColor, wordWrap: { width: 500 } });
+        this.add.text(800, 370, "Click on the trap icon to spawn the trap at the selected location", { font: "25px Arial", fill: heroColor, wordWrap: { width: 500 } });
+        this.add.text(800, 460, "Goal: Stop the Devil from reaching the end of the map in the time limit by setting traps in the level", { font: "25px Arial", fill: heroColor, wordWrap: { width: 500 } });
+
         //button to return to menu
         let backButton = this.utilfunctions.createTextButton(
             80,
